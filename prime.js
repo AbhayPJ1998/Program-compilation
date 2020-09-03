e51b905eeea425d964280ae43ff13e526321d8d1
AbhayPJ83 <70699718+AbhayPJ83@users.noreply.github.com>
2020-09-03 19:11:29 +0530

    Add files via upload

---
+++ prime.js
@@ -0,0 +1,17 @@
+
+    //prime number
+    var n=require('readline-sync')
+    n=n.question('enter number ')     
+    var n, i, flag = true; 
+    for(i = 2; i <= n - 1; i++) 
+        if (n % i == 0) { 
+            flag = false; 
+            break; 
+        } 
+    if (flag == true) {
+        console.log('true')
+    }
+    else{
+        console.log('false')
+    }
+
---
+++ serversub.js
@@ -0,0 +1,7 @@
+var http=require('http')
+http.createServer(server).listen(7000)
+
+function  server(req,res){
+    res.write('Hello ')
+    res.end()
+}
